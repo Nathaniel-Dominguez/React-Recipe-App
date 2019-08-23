10520901af0ff8d76b23d8e5d46324310575f0cf
Nathaniel Dominguez <nathaniel.a.dominguez@gmail.com>
2019-08-23 15:38:05 -0700

    got env working with the app api variable working for requests

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Form from "./components/Form";
+require("dotenv").config();
 
-import Form from './components/Form';
+const API_KEY = process.env.REACT_APP_API_KEY
 
 class App extends Component {
+  getRecipe = async (e) => {
+  	const recipeName = e.target.elements.recipeName.value;
+  	e.preventDefault();
+  	const api_call = await fetch(`https://www.food2fork.com/api/search?key=${API_KEY}&q=shredded%20chicken&count=5`);
+
+  	const data = await api_call.json();
+  	console.log(data);
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Recipe Search</h1>
         </header>
-        <Form />
+        <Form getRecipe={this.getRecipe} />
       </div>
     );
   }
