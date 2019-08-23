dbb2179cd6b9a8e1c994973936801ecdc6795a3e
Nathaniel Dominguez <nathaniel.a.dominguez@gmail.com>
2019-08-23 15:51:45 -0700

    have the recipes saving to state and showing the titles rendered in a paragraph

--- src/App.js
+++ src/App.js
@@ -6,24 +6,31 @@ require("dotenv").config();
 const API_KEY = process.env.REACT_APP_API_KEY
 
 class App extends Component {
-  getRecipe = async (e) => {
-  	const recipeName = e.target.elements.recipeName.value;
-  	e.preventDefault();
-  	const api_call = await fetch(`https://www.food2fork.com/api/search?key=${API_KEY}&q=shredded%20chicken&count=5`);
+	state = {
+		recipes: []
+	}
+	getRecipe = async (e) => {
+		const recipeName = e.target.elements.recipeName.value;
+  		e.preventDefault();
+  		const api_call = await fetch(`https://www.food2fork.com/api/search?key=${API_KEY}&q=shredded%20chicken&count=10`);
 
-  	const data = await api_call.json();
-  	console.log(data);
+  		const data = await api_call.json();
+  		this.setState({ recipes: data.recipes });
+  		console.log(this.state.recipes)
   }
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Recipe Search</h1>
-        </header>
-        <Form getRecipe={this.getRecipe} />
-      </div>
-    );
-  }
+  	render() {
+    	return (
+      	<div className="App">
+        	<header className="App-header">
+        	  <h1 className="App-title">Recipe Search</h1>
+        	</header>
+        	<Form getRecipe={this.getRecipe} />
+        	{ this.state.recipes.map((recipe) => {
+        		return <p>{ recipe.title }</p>
+        	}) }
+      	</div>
+    	);
+  	}
 }
 
 export default App;
