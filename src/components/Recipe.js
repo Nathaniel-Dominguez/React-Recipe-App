bc2ea6a7bdc73a45276ba5370d002a2fca8e5bc9
Nathaniel Dominguez <nathaniel.a.dominguez@gmail.com>
2019-09-26 16:31:09 -0700

    got the state props for single recipes appearing on the page and minor styling

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,11 +1,35 @@
 import React from 'react';
 
+const API_KEY = process.env.REACT_APP_API_KEY
+
 class Recipe extends React.Component {
+	state = {
+		activeRecipe: []
+	}
+	componentDidMount = async () => {
+		const title = this.props.location.state.recipe;
+  		const req = await fetch(`https://www.food2fork.com/api/search?key=${API_KEY}&q=${title}`);
+
+  		const res = await req.json();
+  		this.setState({ activeRecipe: res.recipes[0] });
+  		console.log(this.state.activeRecipe);
+	}
 	render() {
-		console.log(this.props);
+		const recipe = this.state.activeRecipe;
 		return (
-			<div>Recipe Component!</div>
-			);
+			<div className="container">
+				<div className="active-recipe">
+					<img className="active-recipe__img" src={recipe.image_url} alt={recipe.title}/>
+					<h3 className="active-recipe__title">{ recipe.title }</h3>
+					<h4 className="active-recipe__publisher">
+						Publisher: <span>{ recipe.publisher }</span>
+					</h4>
+					<p className="active-recipe__website">Website: <span><a href={recipe.publisher_url}>{recipe.publisher_url}</a></span>
+					</p>
+					<button className="active-recipe__button">Go Home</button>
+				</div>
+			</div>	
+		);
 	}
 };
 
