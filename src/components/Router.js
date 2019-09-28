61e6985add0ef90af073fc6697b6cddf52b2cdc4
Nathaniel Dominguez <nathaniel.a.dominguez@gmail.com>
2019-09-27 17:13:12 -0700

    I needed basename for browserRouter to be the home url in router.js for github pages to deliver the js properly

--- src/components/Router.js
+++ src/components/Router.js
@@ -5,7 +5,7 @@ import App from '../App';
 import Recipe from './Recipe';
 
 const Router = () => (
-	<BrowserRouter>
+	<BrowserRouter basename='https://nathaniel-dominguez.github.io/React-Recipe-App/'>
 		<Switch>
 			<Route path='/' component={App} exact />
 			<Route path='/recipe/:id' component={Recipe} />
